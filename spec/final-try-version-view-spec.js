0fc361f8f6a69e2dd811b5003ac1629e8aa579df
qwerty88T <guiongblacksire@gmail.com>
2022-09-29 20:31:39 +0800

    test3

--- spec/final-try-version-view-spec.js
+++ spec/final-try-version-view-spec.js
@@ -1,9 +1,3 @@
 'use babel';
 
 import FinalTryVersionView from '../lib/final-try-version-view';
-
-describe('FinalTryVersionView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
